b7a7433f5d26c6f7efab6a0dfff2a8cc63578fb3
Camilo Rodríguez <aeroxmotion@hotmail.com>
2018-11-17 22:03:43 -0500

    Missing optional parameters

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,17 @@
 interface Options<T> {
   name: string
-  sync: boolean
-  storage: string
-  preserve: boolean
-  serializer: (entryObject: T) => string
-  deserializer: (entryString: string) => T
+  sync?: boolean
+  storage?: string
+  preserve?: boolean
+  serializer?: (entryObject: T) => string
+  deserializer?: (entryString: string) => T
 }
 
-declare function rs<T>(name: string, entry: T): Proxy<T>
-declare function rs<T>(options: Options<T>, entry: T): Proxy<T>
+declare function rs<T>(name: string, entry?: T): Proxy<T>
+declare function rs<T>(options: Options<T>, entry?: T): Proxy<T>
 
 declare namespace rs {
-  function session<T>(name: string, entry: T): Proxy<T>
+  function session<T>(name: string, entry?: T): Proxy<T>
   function remove<T>(entry: T): void
 }
 
